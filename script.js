901912017251b241faed68a52239224fc0571783
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-09-19 00:34:31 -0300

    Create JS variables

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Testando o arquivo JS");
+let age = document.querySelector('input').value;
+let btn = document.querySelector('button');
+
+let table = document.getElementById('result');
+let avg = document.getElementById('average');
+let total = document.getElementById('total');
+let sale = document.getElementById('sale');
