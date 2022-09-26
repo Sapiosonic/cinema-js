0020e124fabe500dc07eb4607e98c10e88e2e7f4
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-09-26 00:57:01 -0300

    updating

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//seleciona os
 let input = document.querySelector('input');
 let btn = document.querySelector('button');
 
@@ -5,39 +6,48 @@ let table = document.getElementById('result');
 let avg = document.getElementById('average');
 let total = document.getElementById('total');
 let sale = document.getElementById('sale');
+
 const idades = [];
+const descontos = [];
 const valor = 30;
+
 var desconto = 0;
 var soma = 0;
+var final = 0;
 
 function addValor(){
-    let idade = Number(input.value);
-    idades.push(idade);
+
+  let idade = Number(input.value);
+  idades.push(idade);
 
     for(let i = 0; i < idades.length; i++){
 
-    
-        
-        if(idades[i] < 18){
-            desconto = valor - (valor * 0.5);
-        }
-        if (idades[i] >= 18){
-            desconto = valor - (valor * 0.1);
-        }
-        soma += idades[i];
-    }
+      if(idades[i] < 18){
+          desconto = valor - (valor * 0.5);
+          descontos.push(desconto)
+      }
+      if (idades[i] >= 18){
+        desconto = valor - (valor * 0.1);
+        descontos.push(desconto);
+      }
+      final += descontos[i] / idades.length;
+      
 
-    let template = `    
-                    <tr>
+       soma = idades.reduce((a,b) => a + b, 0);
+    }
+         
+    let template = `
                         <td>${idade}</td>
                         <td>${valor}</td>
                         <td>${desconto}</td>
-                    </tr>                        
     `;
+
+    //renderiza o template de tbody N vezes
     table.innerHTML += template;
+    //calcula e renderiza o valor total sem desconto
     total.innerHTML = idades.length * valor;
-    avg.innerHTML = soma / idades.length;
-
+    //renderiza a média das idades apresentadas
+    avg.innerHTML =  soma / idades.length;
+    //calcula e renderiza o valor real arrecadado
+    sale.innerHTML = final;
 };
-
-
